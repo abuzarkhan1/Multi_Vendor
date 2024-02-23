0b4aabf84c00f6eee1d89f2c6e7121a6050ccf83
Abuzar Khan <abuzarkhan1242@gmail.com>
2024-02-24 02:29:13 +0500

    done

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://multi-vendor-qu1p.vercel.app",
     credentials:true
 }));
 
